2e13b10316c265a371c2bf7ddb97635ce98fdcd2
Jeremy Thomas <planetjerm@Jeremys-MacBook-Pro-2.local>
2016-11-19 19:59:57 -0900

    Updates to the regression class. Now available: uncertainties.

--- regression.cpp
+++ regression.cpp
@@ -22,6 +22,16 @@ double Regression::getSlope()
     return _slope;
 }
 
+double Regression::getSlopeUncertainty()
+{
+    return _uncertaintySlope;
+}
+
+double Regression::getInterceptUncertainty()
+{
+    return _uncertaintyIntercept;
+}
+
 int Regression::getSize()
 {
     return _data.size();
@@ -32,140 +42,87 @@ double Regression::getIntercept()
     return _intercept;
 }
 
+double Regression::getStdDev()
+{
+    return _stdDev;
+}
+
 void Regression::sumsCalculator()
 {
-// Sum of all (x_i times y_i)
+
 for (int i=0; i<_data.size(); i++)
 {
+    // Sum of all (x_i times y_i)
     _sumXY = _sumXY + (_data[i].first)*(_data[i].second);
-}
-
-// sum of all x_i
-for (int i=0; i<_data.size(); i++)
-{
+    // sum of all x_i
     _sumX = _sumX + (_data[i].first);
-}
-
-// sum of all y_i
-for (int i=0; i<_data.size(); i++)
-{
+    // sum of all y_i
     _sumY = _sumY + (_data[i].second);
-}
+    // sum of all (x_i)^2
 
-// sum of all (x_i)^2
-for (int i=0; i<_data.size(); i++)
-{
+    // This function is called because the rest of the sums are dependent on it.
+    averagesCalculator();
+
     _sumXsquared = _sumXsquared + std::pow(_data[i].first, 2);
+    // SSyy
+    _SSyy= _SSyy + pow(_data[i].second-_yAve, 2);
+    // SSxy
+    _SSxy= _SSxy + (_data[i].second-_yAve)*(_data[i].first-_xAve);
+    // SSxx
+    _SSxx= _SSxx + pow(_data[i].first-_xAve, 2);
+}
+// End of sumsCalculator
 }
 
-// (sum of all x_i)^2
-double _squaredSumX = std::pow(_sumX, 2);
+void Regression::averagesCalculator()
+{
+    _yAve= (_sumY/_data.size());
+    _xAve= (_sumX/_data.size());
 }
 
 void Regression::slopeCalculator()
 {
-    _slope= ((_data.size()*_sumXY-(_sumX*_sumY))/(_data.size()*_sumXsquared- _squaredSumX));
+    _slope= (_data.size()*_sumXY-_sumX*_sumY)/(_data.size()*_sumXsquared-_sumX*_sumX);
 }
 
 void Regression::stdDevCalculator()
 {
-
+    _stdDev= std::pow((_SSyy-_intercept*_SSxy)/(_data.size()-2) , 1/2);
 }
+
+// parahelium is the minimum distance;
 
 void Regression::interceptCalculator()
 {
-    _intercept=(((_sumXsquared*_sumY)-(_sumXY*_sumX))/(_data.size()*_sumXsquared-_squaredSumX));
+    _intercept= (_sumXsquared*_sumY-_sumXY*_sumX)/(_data.size()*_sumXsquared-_sumX*_sumX);
 }
 
-// double slope()
-// {
-//     int n = data.size();
-    
-//     // Sum of all (x_i times y_i)
-//     double s1= 0.0;
-//     for (int i=0; i<n; i++)
-//     {
-//         s1 = s1 + (data[i].first)*(data[i].second);
-//     }
-
-//     // sum of all x_i
-//     double s2 = 0.0;
-//     for (int i=0; i<n; i++)
-//     {
-//         s2 = s2 + (data[i].x);
-//     }
-
-//     // sum of all y_i
-//     double s3 = 0.0;
-//     for (int i=0; i<n; i++)
-//     {
-//         s3 = s3 + (data[i].y);
-//     }
-
-//     // sum of all (x_i)^2
-//     double s4 = 0.0;
-//     for (int i=0; i<n; i++)
-//     {
-//         s4 = s4 + pow(data[i].x, 2);
-//     }
-
-//     // (sum of all x_i)^2
-//     double s5 = pow(s2, 2);
-
-//     return ((n*s1-s2*s3)/(n*s4-s5));
-// }
-
-// double intercept(const vector <dataPoint> & data)
-// {
-//     int n = data.size();
-    
-//     // Sum of all (x_i times y_i)
-//     double s1= 0.0;
-//     for (int i=0; i<n; i++)
-//     {
-//         s1 = s1 + (data[i].x)*(data[i].y);
-//     }
-
-//     // sum of all x_i
-//     double s2 = 0.0;
-//     for (int i=0; i<n; i++)
-//     {
-//         s2 = s2 + (data[i].x);
-//     }
-
-//     // sum of all y_i
-//     double s3 = 0.0;
-//     for (int i=0; i<n; i++)
-//     {
-//         s3 = s3 + (data[i].y);
-//     }
-
-//     // sum of all (x_i)^2
-//     double s4 = 0.0;
-//     for (int i=0; i<n; i++)
-//     {
-//         s4 = s4 + pow(data[i].x, 2);
-//     }
-
-//     // (sum of all x_i)^2
-//     double s5 = pow(s2, 2);
-
-//     return (((s4*s3)-(s1*s2))/(n*s4-s5));
-// }
-
-int main()
+void Regression::uncertaintiesCalculator()
 {
-    std::vector <std::pair<double, double>> data;
-    
-    // Initalize a test vector;
-    for (double i=0; i<100; i++)
-    {
-        data.push_back(*(new std::pair<double, double> {i,i+1}));
-    }
-    
-    Regression swag(data);
-    
-    std::cout << "Slope: " << swag.getSlope() << std::endl;
-    std::cout << "intercept: " << swag.getIntercept() << std::endl;
-    
+    // The uncertainty in the slope
+    _uncertaintySlope = _stdDev/std::pow(_SSxx, 1/2);
+
+    // The uncertainty in the intercept
+    _uncertaintyIntercept = _stdDev*(1/_data.size()+std::pow(_xAve, 2)/_SSxx);
 }
+
+// int main()
+// {
+//     std::vector <std::pair<double, double>> data;
+    
+//     // Initalize a test vector;
+//     for (double i=0; i<10; i++)
+//     {
+//         data.push_back(std::make_pair(i, i+2));
+//         // std::cout << data[i].first << " " << data[i].second << std::endl;
+//     }
+    
+//     Regression swag(data);
+    
+//     std::cout << "Slope: " << swag.getSlope() << std::endl;
+//     std::cout << "intercept: " << swag.getIntercept() << std::endl;
+//     std::cout << "stdDev: " << swag.getStdDev() << std::endl;
+//     std::cout << "Slope uncertainty: " << swag.getSlopeUncertainty() << std::endl;
+//     std::cout << "Intercept uncertainty: " << swag.getInterceptUncertainty() << std::endl;
+    
+// }
--- regression.hpp
+++ regression.hpp
@@ -11,33 +11,49 @@ class Regression
 {
 public:
     Regression(const std::vector<std::pair<double, double>> & data);
+    
     double getSlope();
+
+    double getSlopeUncertainty();
 
     int getSize();
 
     double getIntercept();
+
+    double getInterceptUncertainty();
+
+    double getStdDev();
 
 
 private:
 
     std::vector<std::pair<double, double>> _data;
     double _slope = 0.0;
+    double _yAve = 0.0;
+    double _xAve = 0.0;
     double _intercept = 0.0;
     double _stdDev = 0.0;
-    double _residual = 0.0;
+    double _SSxy = 0.0;
+    double _SSyy = 0.0;
+    double _SSxx = 0.0;
     double _sumXY = 0.0;          // The sum of (x_i * y_i)
     double _sumX = 0.0;           // The sum of (x_i)
     double _sumY = 0.0;           // The sum of (y_i)
     double _sumXsquared = 0.0;    // The sum of (x_i)^2
-    double _squaredSumX = 0.0;    // The square of the sum of (x_i)
+    double _uncertaintySlope = 0.0;
+    double _uncertaintyIntercept = 0.0;
     
     void sumsCalculator();
+
+    void averagesCalculator();
 
     void slopeCalculator();
 
     void stdDevCalculator();
 
     void interceptCalculator();
+
+    void uncertaintiesCalculator();
 
 };
 
