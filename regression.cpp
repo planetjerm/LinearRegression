c1e69b1896966746891cc67ce1e50d9ab176e4e8
Jeremy Thomas <planetjerm@Jeremys-MacBook-Pro-2.local>
2016-11-19 17:44:44 -0900

    Changed structures to a class

--- regression.cpp
+++ regression.cpp
@@ -3,108 +3,169 @@
 #include "regression.hpp"
 
 #include <iostream>
-    using std::cout; using std::cin; using std::endl;
 #include <fstream>
 #include <string>
-    using std::string; using std::getline;
 #include <vector>
-    using std::vector;
 #include <cmath>
-    using std::pow;
+#include <utility>
 
-
-double slope(const vector <dataPoint> & data)
+Regression::Regression(const std::vector<std::pair<double, double>> & data)
 {
-    int n = data.size();
+    _data=data;
+    sumsCalculator();
+    slopeCalculator();
+    interceptCalculator();
+}
     
-    // Sum of all (x_i times y_i)
-    double s1= 0.0;
-    for (int i=0; i<n; i++)
-    {
-        s1 = s1 + (data[i].x)*(data[i].y);
-    }
-
-    // sum of all x_i
-    double s2 = 0.0;
-    for (int i=0; i<n; i++)
-    {
-        s2 = s2 + (data[i].x);
-    }
-
-    // sum of all y_i
-    double s3 = 0.0;
-    for (int i=0; i<n; i++)
-    {
-        s3 = s3 + (data[i].y);
-    }
-
-    // sum of all (x_i)^2
-    double s4 = 0.0;
-    for (int i=0; i<n; i++)
-    {
-        s4 = s4 + pow(data[i].x, 2);
-    }
-
-    // (sum of all x_i)^2
-    double s5 = pow(s2, 2);
-
-    return ((n*s1-s2*s3)/(n*s4-s5));
+double Regression::getSlope()
+{
+    return _slope;
 }
 
-double intercept(const vector <dataPoint> & data)
+int Regression::getSize()
 {
-    int n = data.size();
-    
-    // Sum of all (x_i times y_i)
-    double s1= 0.0;
-    for (int i=0; i<n; i++)
-    {
-        s1 = s1 + (data[i].x)*(data[i].y);
-    }
-
-    // sum of all x_i
-    double s2 = 0.0;
-    for (int i=0; i<n; i++)
-    {
-        s2 = s2 + (data[i].x);
-    }
-
-    // sum of all y_i
-    double s3 = 0.0;
-    for (int i=0; i<n; i++)
-    {
-        s3 = s3 + (data[i].y);
-    }
-
-    // sum of all (x_i)^2
-    double s4 = 0.0;
-    for (int i=0; i<n; i++)
-    {
-        s4 = s4 + pow(data[i].x, 2);
-    }
-
-    // (sum of all x_i)^2
-    double s5 = pow(s2, 2);
-
-    return (((s4*s3)-(s1*s2))/(n*s4-s5));
+    return _data.size();
 }
 
-// int main()
+double Regression::getIntercept()
+{
+    return _intercept;
+}
+
+void Regression::sumsCalculator()
+{
+// Sum of all (x_i times y_i)
+for (int i=0; i<_data.size(); i++)
+{
+    _sumXY = _sumXY + (_data[i].first)*(_data[i].second);
+}
+
+// sum of all x_i
+for (int i=0; i<_data.size(); i++)
+{
+    _sumX = _sumX + (_data[i].first);
+}
+
+// sum of all y_i
+for (int i=0; i<_data.size(); i++)
+{
+    _sumY = _sumY + (_data[i].second);
+}
+
+// sum of all (x_i)^2
+for (int i=0; i<_data.size(); i++)
+{
+    _sumXsquared = _sumXsquared + std::pow(_data[i].first, 2);
+}
+
+// (sum of all x_i)^2
+double _squaredSumX = std::pow(_sumX, 2);
+}
+
+void Regression::slopeCalculator()
+{
+    _slope= ((_data.size()*_sumXY-(_sumX*_sumY))/(_data.size()*_sumXsquared- _squaredSumX));
+}
+
+void Regression::stdDevCalculator()
+{
+
+}
+
+void Regression::interceptCalculator()
+{
+    _intercept=(((_sumXsquared*_sumY)-(_sumXY*_sumX))/(_data.size()*_sumXsquared-_squaredSumX));
+}
+
+// double slope()
 // {
-//     vector <dataPoint> data;
+//     int n = data.size();
     
-//     // Initalize a test vector;
-//     for (double i=0; i<100; i++)
+//     // Sum of all (x_i times y_i)
+//     double s1= 0.0;
+//     for (int i=0; i<n; i++)
 //     {
-//         data.push_back(*new dataPoint {i,i+1});
+//         s1 = s1 + (data[i].first)*(data[i].second);
 //     }
-    
-//     for(int i=0; i<data.size(); i++)
+
+//     // sum of all x_i
+//     double s2 = 0.0;
+//     for (int i=0; i<n; i++)
 //     {
-//         cout << data[i].y << " and " << data[i].x << endl;
+//         s2 = s2 + (data[i].x);
 //     }
-    
-//     cout << "Slope: " << slope(data) << endl;
-//     cout << "intercept: " << intercept(data) << endl;
-    
+
+//     // sum of all y_i
+//     double s3 = 0.0;
+//     for (int i=0; i<n; i++)
+//     {
+//         s3 = s3 + (data[i].y);
+//     }
+
+//     // sum of all (x_i)^2
+//     double s4 = 0.0;
+//     for (int i=0; i<n; i++)
+//     {
+//         s4 = s4 + pow(data[i].x, 2);
+//     }
+
+//     // (sum of all x_i)^2
+//     double s5 = pow(s2, 2);
+
+//     return ((n*s1-s2*s3)/(n*s4-s5));
 // }
+
+// double intercept(const vector <dataPoint> & data)
+// {
+//     int n = data.size();
+    
+//     // Sum of all (x_i times y_i)
+//     double s1= 0.0;
+//     for (int i=0; i<n; i++)
+//     {
+//         s1 = s1 + (data[i].x)*(data[i].y);
+//     }
+
+//     // sum of all x_i
+//     double s2 = 0.0;
+//     for (int i=0; i<n; i++)
+//     {
+//         s2 = s2 + (data[i].x);
+//     }
+
+//     // sum of all y_i
+//     double s3 = 0.0;
+//     for (int i=0; i<n; i++)
+//     {
+//         s3 = s3 + (data[i].y);
+//     }
+
+//     // sum of all (x_i)^2
+//     double s4 = 0.0;
+//     for (int i=0; i<n; i++)
+//     {
+//         s4 = s4 + pow(data[i].x, 2);
+//     }
+
+//     // (sum of all x_i)^2
+//     double s5 = pow(s2, 2);
+
+//     return (((s4*s3)-(s1*s2))/(n*s4-s5));
+// }
+
+int main()
+{
+    std::vector <std::pair<double, double>> data;
+    
+    // Initalize a test vector;
+    for (double i=0; i<100; i++)
+    {
+        data.push_back(*(new std::pair<double, double> {i,i+1}));
+    }
+    
+    Regression swag(data);
+    
+    std::cout << "Slope: " << swag.getSlope() << std::endl;
+    std::cout << "intercept: " << swag.getIntercept() << std::endl;
+    
+}
--- regression.hpp
+++ regression.hpp
@@ -4,26 +4,49 @@
 #define FILE_REGRESSION_H_INCLUDED
 
 #include <vector>
-	using std::vector;
 #include <string>
-	using std::string;
+#include <utility>
 	
-struct dataPoint
+class Regression
 {
-    double x;
-    double y;
+public:
+    Regression(const std::vector<std::pair<double, double>> & data);
+    double getSlope();
+
+    int getSize();
+
+    double getIntercept();
+
+
+private:
+
+    std::vector<std::pair<double, double>> _data;
+    double _slope = 0.0;
+    double _intercept = 0.0;
+    double _stdDev = 0.0;
+    double _residual = 0.0;
+    double _sumXY = 0.0;          // The sum of (x_i * y_i)
+    double _sumX = 0.0;           // The sum of (x_i)
+    double _sumY = 0.0;           // The sum of (y_i)
+    double _sumXsquared = 0.0;    // The sum of (x_i)^2
+    double _squaredSumX = 0.0;    // The square of the sum of (x_i)
+    
+    void sumsCalculator();
+
+    void slopeCalculator();
+
+    void stdDevCalculator();
+
+    void interceptCalculator();
+
 };
 
-struct graphMetaData
-{
-    string x;
-    string y;
-    string title;
-};
+// struct graphMetaData
+// {
+//     std::string x;
+//     std::string y;
+//     std::string title;
+// };
 
-
-// These functions should return legitimate values.
-double intercept(const vector <dataPoint> & data);
-double slope(const vector <dataPoint> & data);
 
 #endif //#ifndef FILE_BUBBLE_SORT_H_INCLUDED
