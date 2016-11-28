6ce97bdf73e0d1a2a221fb63f99e13088fef2df1
Jeremy Thomas <planetjerm@gmail.com>
2016-11-27 16:26:13 -0900

    Add files via upload

---
+++ boilerplate.cpp
@@ -0,0 +1,10 @@
+/*******************************************************************************
+* Class:            CS 201                                                     *
+* Group Project:    Statistical Analysis                                       *
+* Team Members:     Kelly Offield - kcoffield@alaska.edu                       *
+*                   Jeremy Thomas - jdthomas7@alaska.edu                       *
+*                   Samuel Grenon - sgrenon@alaska.edu                         *
+*                                                                              *
+* File Purpose:     Declaring the Regression class                             *
+* Filename:         regression.hpp                                             *
+*******************************************************************************/
